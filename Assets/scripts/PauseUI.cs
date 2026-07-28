b6a0dd551d10d3948abf2257798309fd0aebc2d7
greatoctober <arianshepetunin@yandex.ru>
2026-07-28 16:33:13 +1000

    28/07

--- Assets/scripts/PauseUI.cs
+++ Assets/scripts/PauseUI.cs
@@ -2,10 +2,12 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseUI : MonoBehaviour
 {
     [SerializeField] private InputActionReference toggle;
+    [SerializeField] private GameObject root; 
     private void OnEnable()
     {
         toggle.action.started += OnToggle;
@@ -17,7 +19,14 @@ public class PauseUI : MonoBehaviour
     {
         if (context.performed)
         {
-            gameObject.SetActive(!gameObject.activeSelf);
+            if (root.activeSelf)
+            {
+                Close();
+            }
+            else
+            {
+                Open();
+            }
         }
     }
 
@@ -29,6 +38,27 @@ public class PauseUI : MonoBehaviour
     }
     public void BackToMainMenu()
     {
+        SceneManager.LoadScene(Scenes.MainMenu);
+
+    }
+
+    public void BackToGame()
+    {
+        Close();
+    }
+
+    private void Open()
+    {
+        root.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0;
+    }
+
+    private void Close()
+    {
+        root.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
 
     }
 }
---
+++ Assets/scripts/Scenes.cs
@@ -0,0 +1,5 @@
+public class Scenes 
+{
+    public const string MainMenu = "MainMenu";
+    public const string Scene1 = "GamePlay";
+}
